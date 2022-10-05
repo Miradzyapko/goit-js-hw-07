2b019493ca09cafcb5e235e77b35a64adff2d42c
Miradzyapko <mira.dzyapko@gmail.com>
2022-10-05 21:08:31 +0300

    змінює код

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -7,15 +7,19 @@ galleryContainer.insertAdjacentHTML("beforeend", imagesMarkup);
 //console.log(galleryContainer)//
 galleryContainer.addEventListener('click', onGalleryContainerClick);
 function createImagesMarkup(galleryItems) {
-return galleryItems.map((item) => {
-    return '<div class = "gallery__item"><a class="gallery__link" href = "${item.original}"><img class="gallery__image" src = "${item.preview}"data-sourse="${item.original}" alt="Image description"/></a></div>';
+    return galleryItems.map(({ preview, original, description }) => {
+        return '<div class ="gallery__item"><a class="gallery__link" href ="${original}"><img class="gallery__image" src ="${preview}" data-sourse="${original}" alt="${description}"/></a></div>';
 }).join('');
 }
 function onGalleryContainerClick(evt) {
 
     evt.preventDefault();
-    if(evt.target.nodeName !=="IMG") 
-    {const instance = basicLightbox.create('<img src = "${evt.target.dataset.source}" width="800" height="600"/>');
-instance.show();
-};
+    if (evt.target.nodeName !== "IMG") {
+        return;
+    }
+
+    {
+        const instance = basicLightbox.create('<img src = "evt.target.dataset.source" width="800" height="600"/>');
+        instance.show();
+    }
 }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -5,25 +5,21 @@ const galleryContainer = document.querySelector(".gallery");
 const imagesMarkup = createImagesMarkup(galleryItems);
 galleryContainer.insertAdjacentHTML("beforeend", imagesMarkup);
 //console.log(galleryContainer)//
-galleryContainer.addEventListener('click', onGalleryContainerClick);
+
 function createImagesMarkup(galleryItems) {
-return galleryItems.map(( {preview, original, description}) => {
-    return '<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}"alt="${description}"/></a></li>';
+return galleryItems.map(({ preview, original, description }) => {
+    return '<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"/></a></li>';
 }).join('');
 }
-function onGalleryContainerClick(evt) {
 
-    evt.preventDefault();
-    if (evt.target.nodeName !== "IMG") 
- return
-    }
-        const lightbox = new SimpleLightbox(".gallery a", {
 
-            captionsData: "alt",
-            captionDelay: 250,
-            captionPosition: "bottom",
-        })
+    const lightbox = new SimpleLightbox(".gallery a", {
 
-     
+        captionsData: "alt",
+        captionDelay: 250,
+        captionPosition: "bottom",
+    })
+
+
 
 
