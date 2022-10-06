d8e4f90bbfde55b457d9ddae4d6efd6401948193
Miradzyapko <mira.dzyapko@gmail.com>
2022-10-07 00:30:20 +0300

    змінює код js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -8,7 +8,7 @@ galleryContainer.insertAdjacentHTML("beforeend", imagesMarkup);
 
 function createImagesMarkup(galleryItems) {
 return galleryItems.map(({ preview, original, description }) => {
-    return '<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"/></a></li>';
+    return `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"/></a></li>`;
 }).join('');
 }
 
